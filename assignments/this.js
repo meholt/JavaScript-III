e29c0c871738a85a091d030eaa025127c98a7cd8
meholt <meholt90@gmail.com>
2019-05-28 20:30:58 -0700

    finished-this-assignment-haha

--- assignments/this.js
+++ assignments/this.js
@@ -15,15 +15,75 @@
 // Principle 1
 
 // code example for Window Binding
+function sayPhrase(name) {
+    return `Good morning, ${this}. Have a good day!`;
+}
+
+console.log(sayPhrase(`Thomas`));
 
 // Principle 2
 
 // code example for Implicit Binding
 
-// Principle 3
+const family = {
+    father: "Thomas",
+    mother: "Megan",
+    oldestDaughter: "Chloe",
+    oldestSon: "James",
+    youngestDaughter: "Hailey",
+    youngestSon: "Adam",
+    familyRelations: function() {
+        return `${this.father} and ${this.mother} are the parents of...`
+    }
+}
 
-// code example for New Binding
+console.log(family.familyRelations());
 
-// Principle 4
+// // Principle 3
 
-// code example for Explicit Binding
+// // code example for New Binding
+
+function FamilyMember(attributes) {
+    this.age = attributes.age;
+    this.name = attributes.name;
+    this.occupation = attributes.occupation;
+    this.goals = attributes.goals;
+    console.log(this);
+}
+
+const mom = new FamilyMember({
+    age: 29,
+    name: "Megan",
+    occupation: "SAHM",
+    goals: "future kick-ass web dev"
+});
+
+const dad = new FamilyMember({
+    age: 30,
+    name: "Thomas",
+    occupation: "chef",
+    goals: "pro gamer"
+});
+
+const child1 = new FamilyMember({
+    age: 5,
+    name: "Chloe",
+    occupation: "complete diva",
+    goals: "princess"
+});
+
+// // Principle 4
+
+// // code example for Explicit Binding
+
+const parents = {
+    parents: "Megan and Thomas"
+}
+
+const children = ["Chloe", "Hailey", "James", "Adam"];
+
+function familyTree(child1, child2, child3, child4) {
+    return `${this.parents} are the parents of ${child1}, ${child2}, ${child3}, ${child4}.`;
+}
+
+console.log(familyTree.call(parents, ...children));

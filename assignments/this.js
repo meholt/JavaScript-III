cbdaca0e036b2d8178ec50852154e068f6625dd2
meholt <meholt90@gmail.com>
2019-05-28 18:37:00 -0700

    finished-short-answers-on-this-assignment

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding: When the value of `this` keyword is that of the window
+* 2. Implicit Binding: This is used for objects and the value of the `this` keyword
+    is dependent upon the object in which it is called/used in.
+* 3. Explicit Binding: This is used for functions and there are three different methods that could be used w/ the `this` keyword:
+    .call, .apply, and .bind.
+* 4. New Binding: This is a constructor function and is used to create a new object. When this is done, the value of `this` keyword 
+    is that of the new object that the constructor function created.
 *
 * write out a code example of each explanation above
 */
